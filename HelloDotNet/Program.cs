10613022c422d3c47094fbf5a604cf25a712d57c
Alexis Georges <alexis.georges@gmail.com>
2015-10-09 14:09:51 -0700

    initial commit

---
+++ HelloDotNet/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LaunchDarkly.Client;
+
+namespace HelloDotNet
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // TODO: Enter your LaunchDarkly API key here
+      LdClient client = new LdClient("YOU_API_KEY");
+      User user = User.WithKey("bob@example.com")
+        .AndFirstName("Bob")
+        .AndLastName("Loblaw")
+        .AndCustomAttribute("groups", "beta_testers");
+
+      // TODO: Enter the key for your feature flag key here
+      var value = client.Toggle("YOU_FEATURE_FLAG_KEY", user, false).ContinueWith((task) =>
+      {
+        if (task.Result)
+        {
+          Console.WriteLine("Showing feature for user");
+        }
+        else
+        {
+          Console.WriteLine("Not showing feature for user");
+        }
+
+        client.Flush();
+      });
+
+      value.Wait();
+
+      Console.WriteLine("Press any key to exit");
+      Console.ReadKey();
+
+    }
+  }
+}
